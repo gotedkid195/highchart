da9321ca1271adea052fb7fb7f56e23700c031c1
gotedkid195 <40803862+gotedkid195@users.noreply.github.com>
2018-07-12 18:48:13 +0700

    Update and rename vv.php to data_tem.php

--- data_tem.php
+++ data_tem.php
@@ -1,20 +1,23 @@
 <?php 
+//include('inc/myconnect.php');
 $dbc= mysqli_connect('localhost','root','','test');
 mysqli_set_charset($dbc, 'UTF8');
 if (!$dbc) {
     die("Kết nối thất bại: " . mysqli_connect_error());
 }
-
-$sql ="SELECT * FROM nhietdo  ORDER BY id DESC LIMIT 10";
+    
+$sql ="SELECT ngaygio,tem FROM clno  ORDER BY id DESC LIMIT 1000";
 $fetch = mysqli_query($dbc,$sql); 
 
 while ($row = mysqli_fetch_array($fetch, MYSQLI_ASSOC)) {
+
     extract($row);
-    $json[]=[strtotime($ngaygio),(int)$tem];
+
+    $json[]=[strtotime($ngaygio)*1000,(float)$tem];
 }
 
 echo json_encode($json);
-//KẾT QUẢ ECHO
-//[[1531314325,33],[1531314319,43],[1531314313,23],[1531314309,23],[1531314306,23]]
-mysqli_close($dbc);	
+
+  die;
+//}
  ?>
